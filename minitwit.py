66d392a26fcf113df46e1db033321bc4785643d4
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-02-01 14:09:32 +0100

    Update with 2to3

--- minitwit.py
+++ minitwit.py
@@ -8,7 +8,7 @@
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-from __future__ import with_statement
+
 import re
 import time
 import sqlite3
@@ -39,7 +39,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().executescript(f.read().decode("UTF-8"))
         db.commit()
 
 
@@ -94,7 +94,7 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    print("We got a visitor from: " + str(request.remote_addr))
+    print(("We got a visitor from: " + str(request.remote_addr)))
     if not g.user:
         return redirect(url_for('public_timeline'))
     offset = request.args.get('offset', type=int)
