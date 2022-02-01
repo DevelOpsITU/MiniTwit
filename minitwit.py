80a9f15e44963528654bdddc42c9ad81c79dad1b
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-02-01 14:09:49 +0100

    update with 2 to3

--- minitwit.py
+++ minitwit.py
@@ -94,7 +94,7 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    print(("We got a visitor from: " + str(request.remote_addr)))
+    print("We got a visitor from: " + str(request.remote_addr))
     if not g.user:
         return redirect(url_for('public_timeline'))
     offset = request.args.get('offset', type=int)
