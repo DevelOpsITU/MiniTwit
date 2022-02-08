bd6922b1878b87e4a2e72b4435bbc2b3703d57cd
ValarMarkhulis <christian.s.mark@hotmail.com>
2022-02-08 18:30:00 +0100

    Gonja/jinja "extends" keyword works

--- src/minitwit.go
+++ src/minitwit.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 )
 
+func addNumbers(n1 int, n2 int) int {
+	return n1 + n2
+}
+
 // Pre-compiling the templates at application startup using the
 // little Must()-helper function (Must() will panic if FromFile()
 // or FromString() will return with an error - that's it).
 // It's faster to pre-compile it anywhere at startup and only
 // execute the template later.
+
+var tpl_append = gonja.Must(gonja.FromFile("templates/append.html"))
 var tpl = gonja.Must(gonja.FromFile("templates/example.html"))
 
 func examplePage(w http.ResponseWriter, r *http.Request) {
@@ -23,11 +29,13 @@ func examplePage(w http.ResponseWriter, r *http.Request) {
 	g.user = true
 	g.name = "jonas"
 
-	out, err := tpl.Execute(gonja.Context{"first_name": "Christian", "last_name": "Mark", "g": g})
+	out2, err2 := tpl.Execute(gonja.Context{"first_name": "Christian", "last_name": "Mark", "g": g})
+	out, err := tpl_append.Execute(gonja.Context{"first_name": "Christian", "last_name": "Mark", "g": g})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	w.Write([]byte(out))
+	print(out, err2)
+	w.Write([]byte(out2))
 }
 
 func main() {
