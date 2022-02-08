8ee4463e25083d46e8a17b91c1b2f6cdb76a77b3
Kaare Børsting <boer@itu.dk>
2022-02-08 17:11:42 +0100

    db restore & db setup

--- minitwit.go
+++ minitwit.go
@@ -10,10 +10,19 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const DATABASE = "tmp/minitwit.db"
+//const DATABASE = "tmp/minitwit.db"
+const DATABASE = "C:/Users/hardk/source/repos/MiniTwit/minitwit.db"
 const PER_PAGE = 30
 const DEBUG = true
 const SECRET_KEY = "development key"
+
+type Message struct {
+	MessageId int
+	AuthorId  int
+	Text      string
+	Pubdate   int64
+	Flagged   bool
+}
 
 /****************************************
 *			DATABASE RELATED			*
@@ -23,7 +32,7 @@ func connectDb() *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+
 	return db
 }
 
@@ -34,16 +43,34 @@ func initDb() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(query)
 	if _, err := db.Exec(string(query)); err != nil {
 		panic(err)
 	}
-	val, err := db.Query("SELECT table_name FROM all_tables;")
-	fmt.Println(val)
 }
 
-/*func queryDb(query, args, one) {
-	cur :=
-}*/
+// example database usage
+func getAllMessages() {
+	db := connectDb()
+	query := "SELECT * FROM message"
+	result, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer result.Close()
+
+	var messages []Message
+
+	for result.Next() {
+		var msg Message
+		err := result.Scan(&msg.MessageId, &msg.AuthorId, &msg.Text, &msg.Pubdate, &msg.Flagged)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(msg.Text)
+		messages = append(messages, msg)
+	}
+}
 
 /****************************************
 *			 	HANDLERS				*
@@ -61,6 +88,6 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/deez", deez)
 	http.Handle("/", r)
-	initDb()
+	getAllMessages()
 	http.ListenAndServe(":8080", r)
 }
