5c562865d4770de1513a15fd544583a2b0ca858b
Thomsen96 <jonastoftethomsen@gmail.com>
2022-02-08 16:28:04 +0100

    Get a template working with serving html from Gin

--- src/example.go
+++ src/example.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
