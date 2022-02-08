02a2c0711686fc2f61406c3e1e89eb182874d42f
Thomsen96 <jonastoftethomsen@gmail.com>
2022-02-08 16:01:18 +0100

    Add Go Gin

---
+++ src/example.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
