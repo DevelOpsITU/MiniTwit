9210e84b2b9e00ab6cb793fc8ef37c6fba61859b
Lucki2g <hardkaare123@gmail.com>
2022-02-09 13:22:09 +0100

    play with pongo2 (template language like gonga)

--- minitwit.go
+++ minitwit.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/flosch/pongo2/v5" // antivirus does not seem to like this
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -15,6 +16,9 @@ const PER_PAGE = 30
 const DEBUG = true
 const SECRET_KEY = "development key"
 
+/****************************************
+*		   DATABASE ENTITIES			*
+****************************************/
 type Message struct {
 	MessageId int
 	AuthorId  int
@@ -71,6 +75,18 @@ func getAllMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 /****************************************
+*		  		 PONGO2					*
+****************************************/
+var tplExample = pongo2.Must(pongo2.FromFile("templates/test.html"))
+
+func testPage(w http.ResponseWriter, r *http.Request) {
+	err := tplExample.ExecuteWriter(pongo2.Context{"first_name": r.FormValue("deez?")}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+/****************************************
 *			 	HANDLERS				*
 ****************************************/
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -87,11 +103,22 @@ func main() {
 
 	r.HandleFunc("/hello", hello)
 
+	// pongo2 template testpage
+	r.HandleFunc("/test", testPage)
+
 	// Serve static files (css)
 	fileServer := http.FileServer(http.Dir("static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
 
 	// Serve index page on all unhandled routes
+	r.PathPrefix("/login").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./templates/login.html")
+	})
+
+	r.PathPrefix("/register").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./templates/register.html")
+	})
+
 	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./templates/timeline.html")
 	})
