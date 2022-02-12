4b6a679341e831decb04287f8bd59e56fc513dbe
ValarMarkhulis <christian.s.mark@hotmail.com>
2022-02-12 17:51:33 +0100

    Cookies work a little bit

--- src/minitwit.go
+++ src/minitwit.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,17 +21,39 @@ func addNumbers(n1 int, n2 int) int {
 
 var tpl = gonja.Must(gonja.FromFile("templates/example.html"))
 
-func examplePage(w http.ResponseWriter, r *http.Request) {
+func examplePage(w http.ResponseWriter, r *http.Request, c *gin.Context) {
 	// Execute the template per HTTP request
-
 	type User struct {
-		username string
+		Username string
 	}
 	type structType struct {
-		user User
+		User     User
+		Message  bool
+		Messages []string
 	}
 	var g structType
-	//g.user.username = "jonas"
+	g.User.Username = "jonas"
+
+	cookie, err := c.Cookie("session")
+
+	// If there is no cookie
+	if err != nil {
+		cookie = "NotSet"
+
+		data, err := json.Marshal(g)
+		if err != nil {
+			return
+		}
+		print(string(data))
+		c.SetCookie("session", string(data), 3600, "/", "localhost", false, true)
+	} else {
+		json.Unmarshal([]byte(cookie), &g)
+
+	}
+
+	fmt.Printf("Cookie value: %s \n", cookie)
+
+	//set g = "None" if g.user should return false in jinja
 
 	out, err := tpl.Execute(gonja.Context{"first_name": "Christian", "last_name": "Mark", "g": g})
 	if err != nil {
@@ -50,7 +74,7 @@ func main() {
 
 	router.Static("/static", "./static")
 	router.GET("/", func(c *gin.Context) {
-		examplePage(c.Writer, c.Request)
+		examplePage(c.Writer, c.Request, c)
 	})
 	router.LoadHTMLFiles("./src/test.html")
 
