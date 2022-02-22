32e8f1a63a551edfca07542b0ddc3db34e9c62f7
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-02-22 14:23:18 +0100

    Create a build file for our go app, that takes a outputfilename as an argument. The file will exit with a non-zero code if the build went bad.

---
+++ build_app.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Running from ${0%/*}"
+
+echo "Built and tested on 'go version go1.17.6 linux/amd64'"
+go_version=$(go version)
+echo "You have version:   '$go_version'"
+echo
+
+DEFAULT_FILENAME="group_d_go_app"
+filename=$DEFAULT_FILENAME
+
+if [[ -z $1 ]];
+then
+    echo "${0##*/}:  No parameter passed: using '$DEFAULT_FILENAME' as output name for go output file"
+else
+    echo "Using '$1' as output name for go output file"
+    filename=$1
+fi
+
+
+echo "Running build command: 'build -o out/\"$filename\" src/minitwit.go'"
+go build -o out/"$filename" src/minitwit.go
+res=$?
+echo
+
+# Colors from https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
+if [ $res -eq 0 ]
+then
+    GREEN='\033[0;32m'
+    echo -e "${GREEN}Built file \"$filename\" in /out folder successfully"
+    exit 0
+else
+    RED='\033[0;31m'
+    echo -e "${RED}PANIC, something was wrong with the compilation!"
+    exit 1
+fi
