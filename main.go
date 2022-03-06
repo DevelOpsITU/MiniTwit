06b476799c8e3efd2947ef05d984bc2746885079
Thomsen96 <jonastoftethomsen@gmail.com>
2022-03-06 15:53:57 +0100

    Add setups to generate mock data

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"gorm.io/driver/sqlite"
 	"minitwit/config"
 	"minitwit/controllers"
 	"minitwit/database"
@@ -13,15 +11,8 @@ func main() {
 	// Get configuration
 	config.SetupConfig()
 
-	//database.TestConnection()
-	//TODO: Test if the database connection can be established
-	_, err := database.InitGorm(sqlite.Open(config.GetConfig().Database.ConnectionString))
-	if err != nil {
-		if err != nil {
-			fmt.Println(err.Error())
-			panic("failed to connect database")
-		}
-	}
+	database.Init()
+
 	// Blocking call in router.run
 	controllers.HandleRESTRequests()
 }
