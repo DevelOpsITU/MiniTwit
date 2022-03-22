61881de61a839e0b2d922a0c82319cb39fbe62d7
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-03-22 14:35:54 +0100

    Final docker-release change

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -15,16 +15,26 @@ else
 fi
 
 
+
 docker login -u "$username" -p "$password"
 
 BINARY_NAME=minitwit-go-dev
 DOCKER_REGISTRY=groupddevops/
 VERSION=$(git rev-parse --short HEAD)
 
-echo $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:$VERSION
+echo "pushing:" $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:$VERSION "to dockerhub"
 
-#docker tag $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:latest
-#docker tag $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:$VERSION
+
+docker tag $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:$VERSION
 # Push the docker images
-#docker push $DOCKER_REGISTRY$BINARY_NAME:latest
-#docker push $DOCKER_REGISTRY$BINARY_NAME:$VERSION
+docker push $DOCKER_REGISTRY$BINARY_NAME:$VERSION
+
+if [[ -z $3 ]];
+then
+    echo "Done"
+else
+    echo "production release choosen, also tagging with latest"
+    docker tag $BINARY_NAME $DOCKER_REGISTRY$BINARY_NAME:latest
+    docker push $DOCKER_REGISTRY$BINARY_NAME:latest
+    echo "Done"
+fi
